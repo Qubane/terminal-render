c623836c0d12ae44558abd3ae4861a893fffb286
UltraQbik <no1skill@yandex.ru>
2024-05-03 00:55:25 +0300

    Working high res BW output

--- src/window.py
+++ src/window.py
@@ -60,22 +60,22 @@ class Window:
         cls._mode = mode
 
         # any kind of color mode cannot work with high_res flag
-        if mode is not Mode.bw and mode is Mode.high_res:
+        if not mode & Mode.bw and mode & Mode.high_res:
             raise NotImplementedError("Unable to have color with high_res flag enabled")
 
         # update virtual width and height
-        cls._width = cls._terminal_width if mode is not Mode.high_res else cls._terminal_width * 2
-        cls._height = cls._terminal_height if mode is not Mode.high_res else cls._terminal_height * 4
+        cls._width = cls._terminal_width if not mode & Mode.high_res else cls._terminal_width * 2
+        cls._height = cls._terminal_height if not mode & Mode.high_res else cls._terminal_height * 4
 
         # initialize the image buffer
         cls._disp_buffer = [0 for _ in range(cls._width * cls._height)]
 
         # initialize palette
-        if cls._mode is Mode.monochrome:
+        if cls._mode & Mode.monochrome:
             cls.palette = copy(Palette.monochrome)
-        elif cls._mode is Mode.palette4:
+        elif cls._mode & Mode.palette4:
             cls.palette = copy(Palette.palette4)
-        elif cls._mode is Mode.palette8:
+        elif cls._mode & Mode.palette8:
             cls.palette = copy(Palette.palette8)
         else:  # full color or BW
             cls.palette = []
@@ -96,8 +96,33 @@ class Window:
         output = '\33[H'
 
         # update for BW terminal
-        if cls._mode is Mode.bw:
-            rows = [cls._disp_buffer[i*cls._width:i*cls._width+cls._width] for i in range(cls._height)]
-            for row in rows:
-                output += ''.join(map(lambda x: ' ' if x == 0 else '#', row))
+        if cls._mode & Mode.bw:
+            if not cls._mode & Mode.high_res:
+                rows = [cls._disp_buffer[i*cls._width:i*cls._width+cls._width] for i in range(cls._height)]
+                for row in rows:
+                    output += ''.join(map(lambda x: ' ' if x == 0 else '#', row))
+            else:
+                p = lambda x: cls._disp_buffer[x]
+                for yo in range(0, cls._height, 4):
+                    for xo in range(0, cls._width, 2):
+                        # calculate index
+                        idx = xo + yo * cls._width
+
+                        # first row
+                        braille = p(idx)
+                        braille += p(idx + 1) << 3
+
+                        # second row
+                        braille += p(idx + cls._width) << 1
+                        braille += p(idx + 1 + cls._width) << 4
+
+                        # third row
+                        braille += p(idx + cls._width * 2) << 2
+                        braille += p(idx + 1 + cls._width * 2) << 5
+
+                        # forth row
+                        braille += p(idx + cls._width * 3) << 6
+                        braille += p(idx + 1 + cls._width * 3) << 7
+
+                        output += chr(braille + 0x2800)
             print(output, end='', flush=True)
