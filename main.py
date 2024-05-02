3cc24e6da91a24123f8c0a98702f96bc09823536
UltraQbik <no1skill@yandex.ru>
2024-05-03 00:57:47 +0300

    Make pretty sinusoid output

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import math
 from src import *
 from time import sleep
 
 
 def main():
     win = Window()
-    win.initialize(Mode.bw)
+    win.initialize(Mode.bw | Mode.high_res)
+
     count = 0
     while True:
         win.update()
-        for i in range(3600):
-            win._disp_buffer[i] = (i + count + (i // 120)) % 6
+        win.clear()
+        for i in range(win.width):
+            val = (math.sin(i / 20 + (count / 10)) + 1) / 2
+            y = int(val * win.height)
+            win._disp_buffer[y * win.width + i] = 1
         count += 1
-        # sleep(0.0333)  # ~1/30
+        sleep(0.0333)  # ~1/30
 
 
 if __name__ == '__main__':
