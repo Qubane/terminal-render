0187b7c3dba6b0e9f5fa7c5528c9ac046bee9a87
UltraQbik <no1skill@yandex.ru>
2024-05-04 16:27:40 +0300

    Fix test

--- test.py
+++ test.py
@@ -1,25 +1,22 @@
 import math
-from terminal_render import *
 from time import sleep
+from TerminalRender import *
 
 
 def main():
     win = Window()
-    # win.initialize(Mode.bw | Mode.high_res)
-    # win.initialize(Mode.bw)
-    win.initialize(Mode.palette8)
+    win.initialize(Mode.rgb)
 
     count = 0
     while True:
         win.update()
-        # win.clear()
-        # for i in range(win.width):
-        #     val = (math.sin(i / 20 + (count / 10)) + 1) / 2
-        #     y = int(val * win.height * 0.8) + 1
-        #     win.plot(i, y)
-        win.buffer[count % len(win.buffer)] = count // 2
+        win.clear()
+        for i in range(win.width):
+            val = (math.sin(i / 20 + (count / 10)) + 1) / 2
+            y = int(val * win.height * 0.8) + 1
+            win.plot(i, y, int(val * 255) << 8)
         count += 1
-        # sleep(0.0333)  # ~1/30
+        sleep(0.0333)  # ~1/30
 
 
 if __name__ == '__main__':
